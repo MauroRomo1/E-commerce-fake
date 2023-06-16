4b4189c078affb3556db43edaa540edaf9aae911
Mauro Romo <mauroromo.developer@outlook.com>
2023-06-16 18:54:03 -0300

    Cambiando la img de login y agregando un setTimeout para cuando se loguean

--- js/login.js
+++ js/login.js
@@ -20,6 +20,7 @@ const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [
 ];
 
 const formLogin = document.querySelector("#login");
+const loginBtn = document.querySelector("#loginBtn");
 const userEmail = document.querySelector("#userEmail");
 const userPassword = document.querySelector("#userPassword");
 const alertError = document.querySelector("#alertError");
@@ -63,12 +64,24 @@ const logueoUser = () => {
   ) {
     alertError.classList.add("d-none");
     let { id, role, nombre, apellido, edad, email } = usuarioEncontrado;
-    console.log({ id, role, nombre, apellido, edad, email });
     localStorage.setItem(
       "userLogueado",
       JSON.stringify({ id, role, nombre, apellido, edad, email })
     );
-    location.replace("../index.html");
+    loginBtn.setAttribute("disabled", "");
+    loginBtn.innerHTML =
+      /* HTML */
+      `
+        <span
+          class="spinner-grow spinner-grow-sm"
+          role="status"
+          aria-hidden="true"
+        ></span>
+        Cargando...
+      `;
+    setTimeout(() => {
+      location.replace("../index.html");
+    }, 1000);
   } else {
     alertError.classList.remove("d-none");
     userPassword.value = "";
